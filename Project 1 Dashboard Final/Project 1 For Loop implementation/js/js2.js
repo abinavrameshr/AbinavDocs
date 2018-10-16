97a90fb6a6b572f6f0e173cb0b371bfac72e7b0a
abinavrameshr <43980144+abinavrameshr@users.noreply.github.com>
2018-10-16 22:29:57 +0530

    Add files via upload

---
+++ Project 1 Dashboard Final/Project 1 For Loop implementation/js/js2.js
@@ -0,0 +1,50 @@
+function sample2(){
+
+function tablecreate() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var response = JSON.parse(xhttp.responseText);
+        var container1 = response.container1;
+      
+            var table = '<tr><th>' + container1[0].container.th1 + '</th>' +
+            
+            '<th>' + container1[0].container.th3 + '</th>' +
+            
+            '<th>' + container1[0].container.th5 + '<th></th></tr></thead>';
+
+           
+            for(var r = 1; r < container1.length; r++){
+                
+                var data = [];
+                data[0] = '<tr><td><p style="color:white">'+container1[r].container.tr1+'</p>' +'<p>'+ container1[r].container.tr2+ '</p></td>';
+                
+                data[2] ='<td class="member">'+'<figure><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/584938/people_8.png"/></figure>'+'<div class="member-info"><p>'+container1[r].container.tr5+'</p><p>'+container1[r].container.tr6+'</p></div></td>';
+                
+                data[4] = '<td class="status"><span class="status-text status-green">Complete</span><form class="form" action="#" method="POST"><select class="action-box"><option>Actions</option><option>Start project</option><option>Send for QA</option><option>Send invoice</option></select></form></td></tr>'                    
+                
+                                 
+                                    
+                for(var c = 0; c < data.length; c++) {
+                        table += data[c];
+                    
+                }
+                
+                
+            
+                        
+            } 
+           
+          document.getElementById("tablecreate").innerHTML = table; 
+          console.log(table);
+                              
+                                
+        }
+     };
+        xhttp.open("GET", "js/Json2.json", true);
+        xhttp.send();  
+    }
+    
+    document.getElementById("tablecreate").innerHTML= tablecreate();
+
+}
